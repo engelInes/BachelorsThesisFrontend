484cb6c428427c3dc30b5b0460ae7bc91cb40bd9
inesEngel <ines.engel@stud.ubbcluj.ro>
2025-03-16 16:25:29 +0200

    feature/user-page implemented saving image and taking screenshot functionality

--- FE/airport-baggage-detection-system/app/(tabs)/index.tsx
+++ FE/airport-baggage-detection-system/app/(tabs)/index.tsx
@@ -1,15 +1,32 @@
 import Button from "@/components/Buttons";
+import CircleButton from "@/components/CircleButton";
+import EmojiList from "@/components/EmojiList";
+import EmojiPicker from "@/components/EmojiPicker";
+import EmojiSticker from "@/components/EmojiSticker";
+import IconButton from "@/components/IconButton";
 import ImageViewer from "@/components/ImageViewer";
+import { type ImageSource } from "expo-image";
 import * as ImagePicker from "expo-image-picker";
-import { useState } from "react";
+import * as MediaLibrary from "expo-media-library";
+import { useRef, useState } from "react";
 import { StyleSheet, View } from "react-native";
+import { captureRef } from "react-native-view-shot";
 
 const PlaceholderImage = require("../../assets/images/image.png");
 
 export default function Index() {
+  const imageRef = useRef<View>(null);
+  const [status, requestPermisiion] = MediaLibrary.usePermissions();
   const [selectedImage, setSelectedImage] = useState<string | undefined>(
     undefined
   );
+
+  const [showAppOptions, setShowAppOption] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(
+    undefined
+  );
+
   const pcikImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
@@ -23,16 +40,77 @@ export default function Index() {
       alert("Please select an image");
     }
   };
+
+  const onReset = () => {
+    setShowAppOption(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const onSaveImageAsync = async () => {
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1,
+      });
+
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if (localUri) {
+        alert("Saved!");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  if (status == null) {
+    requestPermisiion();
+  }
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        {/* hardcoded <Image source={PlaceholderImage} style={styles.image} /> */}
-        <ImageViewer imgSource={selectedImage || PlaceholderImage} />
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer imgSource={selectedImage || PlaceholderImage} />
+          {pickedEmoji && (
+            <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+          )}
+        </View>
       </View>
-      <View style={styles.footerContainer}>
-        <Button onPress={pcikImageAsync} label="Choose photo" theme="primary" />
-        <Button label="Use this photo" />
-      </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton
+              icon="save-alt"
+              label="Save"
+              onPress={onSaveImageAsync}
+            />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            onPress={pcikImageAsync}
+            label="Choose photo"
+            theme="primary"
+          />
+          <Button
+            label="Use this photo"
+            onPress={() => setShowAppOption(true)}
+          />
+        </View>
+      )}
+      ;
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
     </View>
   );
 }
@@ -63,5 +141,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: "center",
+  },
+  optionsContainer: {
+    position: "absolute",
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: "center",
+    flexDirection: "row",
   },
 });
---
+++ FE/airport-baggage-detection-system/components/EmojiList.tsx
@@ -0,0 +1,54 @@
+import { Image, type ImageSource } from "expo-image";
+import { useState } from "react";
+import { FlatList, Platform, Pressable, StyleSheet } from "react-native";
+
+type Props = {
+  onSelect: (image: ImageSource) => void;
+  onCloseModal: () => void;
+};
+
+export default function EmojiList({ onSelect, onCloseModal }: Props) {
+  const [emoji] = useState<ImageSource[]>([
+    require("../assets/images/image.png"),
+    require("../assets/images/image.png"),
+    require("../assets/images/image.png"),
+    require("../assets/images/image.png"),
+    require("../assets/images/image.png"),
+    require("../assets/images/image.png"),
+  ]);
+
+  return (
+    <FlatList
+      horizontal
+      showsHorizontalScrollIndicator={Platform.OS === "web"}
+      data={emoji}
+      contentContainerStyle={styles.listContainer}
+      renderItem={({ item, index }) => (
+        <Pressable
+          onPress={() => {
+            onSelect(item);
+            onCloseModal();
+          }}
+        >
+          <Image source={item} key={index} style={styles.image} />
+        </Pressable>
+      )}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  listContainer: {
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginRight: 20,
+  },
+});
